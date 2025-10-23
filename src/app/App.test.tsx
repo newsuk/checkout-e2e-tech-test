da84a2e87296bcced46aad6d35f4900cb5f3e8ea
Callum Christie <callumchristie@hotmail.com>
2025-10-23 09:04:12 +0100

    update readme (#6)

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,10 +1,10 @@
 import { test } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
 import App from "./App.tsx";
 
 test("renders the App component", () => {
   render(<App />);
-  // screen.debug();
+  screen.getByText(/Account set up/i);
 });
