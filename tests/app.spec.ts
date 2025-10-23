d86ec9ca9fc9b2e36499d2c624c286a884b0e73d
Callum <callumchristie@hotmail.com>
2025-10-23 06:34:14 +0100

    update readme

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("account set up page displayed", async ({ page }) => {
+test("account set up page", async ({ page }) => {
   await page.goto("http://localhost:5173");
   await expect(page).toHaveTitle(/Account set up/);
 });
